f3383f5c664758200fe12dfea2087c5adc997408
AnthonyCatalfo <anthonycatalfo@gmail.com>
2018-11-21 21:32:46 -0500

    Updating Checkout 2

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -55,7 +55,7 @@ const Checkout = class extends React.Component {
       description: 'A product well worth your time',
       token: token => {
         fetch(
-          "https://distracted-ritchie-5c9292.netlify.com/.netlify/functions/opurchase",
+          "https://distracted-ritchie-5c9292.netlify.com/.netlify/functions/purchase",
           {
             method: 'POST',
             body: JSON.stringify({
